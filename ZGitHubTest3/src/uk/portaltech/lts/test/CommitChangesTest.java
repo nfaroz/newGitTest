aa0300f3fd270e297168addccb7db8fda8c684f6
nfaroz <n.faroz@replyltd.co.uk>
2015-08-05 14:54:36 +0100

    Created class

---
+++ ZGitHubTest3/src/uk/portaltech/lts/test/CommitChangesTest.java
@@ -0,0 +1,9 @@
+package uk.portaltech.lts.test;
+
+import java.util.MissingFormatArgumentException;
+
+public class CommitChangesTest {
+	public static void main(String[] args) {
+		System.out.println("Documenting commit");
+	}
+}
