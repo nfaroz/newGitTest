9243e705da2d25db5b3016ac9e757c661b56f830
nfaroz <n.faroz@replyltd.co.uk>
2015-08-05 10:26:41 +0100

    New Project ZGitHubTest
    Signed-off-by: nfaroz <n.faroz@replyltd.co.uk>

---
+++ ZGitHubTest/src/uk/portaltech/lts/test/NewUpdate.java
@@ -0,0 +1,8 @@
+package uk.portaltech.lts.test;
+
+public class NewUpdate {
+
+	public static void main(String[] args) {
+		System.out.println("New Java class");
+	}
+}
---
+++ ZGitHubTest/src/uk/portaltech/lts/test/TestingGit.java
@@ -0,0 +1,10 @@
+package uk.portaltech.lts.test;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing git");
+	}
+
+}
