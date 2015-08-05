206ad65b86c84615277f4e1e39846dbf7486c6d7
nfaroz <n.faroz@replyltd.co.uk>
2015-08-05 14:03:34 +0100

    Changed multipleGitProjects

--- ZGitHubTest2/src/uk/portaltech/lts/test/MultipleGItProjects.java
+++ ZGitHubTest2/src/uk/portaltech/lts/test/MultipleGItProjects.java
@@ -3,6 +3,6 @@ package uk.portaltech.lts.test;
 public class MultipleGItProjects {
 
 	public static void main(String[] args) {
-		System.out.println("Testing second Git Project");
+		System.out.println("Testing second Git Project. asd");
 	}
 }
