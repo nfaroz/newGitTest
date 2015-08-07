84980412b024b5aff6bcd846e2a0b43cbccfc38d
nfaroz <n.faroz@replyltd.co.uk>
2015-08-07 12:21:06 +0100

    Update TestingGit.java

--- ZGitHubTest/src/uk/portaltech/lts/test/TestingGit.java
+++ ZGitHubTest/src/uk/portaltech/lts/test/TestingGit.java
@@ -4,7 +4,7 @@ public class TestingGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("testing git project Again");
+		System.out.println("testing git project Again n Again");
 	}
 
 }
